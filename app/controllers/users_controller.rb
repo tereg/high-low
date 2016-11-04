a359a04106e7d4484af7f240940b1538fafdbddc
tereg <teregalla@gmail.com>
2016-11-04 15:21:09 -0700

    Add users new, create, and show routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,38 +5,38 @@
 #   erb :'users/index'
 # end
 
-# # # USERS NEW
-# # get '/users/new' do
-#   @user = User.new
-#   erb :'users/new'
-# end
+# # USERS NEW
+get '/users/new' do
+  @user = User.new
+  erb :'users/new'
+end
 
 # USERS CREATE
-# post '/users' do
+post '/users' do
 
-#   if params[:password_confirmation] == params[:user][:password]
-#     @user = User.new(params[:user])
+  if params[:password_confirmation] == params[:user][:password]
+    @user = User.new(params[:user])
 
-#     if @user.save
-#       session[:id] = @user.id
-#       redirect "/users/#{@user.id}"
-#     else
-#       @errors = @user.errors.full_messages
-#       erb :'users/new'
-#     end
+    if @user.save
+      session[:id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      @errors = @user.errors.full_messages
+      erb :'users/new'
+    end
 
-#   else
-#     @errors = ["Passwords do not match!"]
-#     erb :'users/new'
-#   end
+  else
+    @errors = ["Passwords do not match!"]
+    erb :'users/new'
+  end
   
-# end
+end
 
 # # USERS SHOW
-# get '/users/:id' do
-#   @user = User.find(params[:id])
-#   erb :'users/show'
-# end
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
 
 # # USERS EDIT
 # get '/users/:id/edit' do
