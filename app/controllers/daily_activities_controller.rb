ad28cfef9934e21c285404d343e57a9a479f38bd
tereg <teregalla@gmail.com>
2016-11-05 12:32:45 -0700

    Create form to add highs and lows, update daily activities post route, and add link to highs and lows form to user show page

--- app/controllers/daily_activities_controller.rb
+++ app/controllers/daily_activities_controller.rb
@@ -8,3 +8,14 @@ get '/users/:user_id/daily_activities/new' do
   @user = User.find(params[:user_id])
   erb :'partials/_daily_activities_form' 
 end 
+
+post '/users/:user_id/daily_activities' do 
+  @user = User.find(params[:user_id])
+  @daily_activity = DailyActivity.new({user_id: current_user.id, highs: params[:highs], lows: params[:lows]})
+
+  if @daily_activity.save
+    redirect "/users/#{@user.id}"
+  else 
+    erb :'partials/_daily_activities_form' 
+  end 
+end 
