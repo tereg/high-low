134b5841a729221972ca7d677e878ed79f78817c
tereg <teregalla@gmail.com>
2017-03-10 11:06:08 -0800

    Update UI
    Add cloud video background to landing page and cloud background to other pages.
    
    Update font and login and register buttons.
    
    Update format of ajaxed login form.

--- public/js/application.js
+++ public/js/application.js
@@ -16,7 +16,7 @@ var clickLoginListener = function() {
 
   request.done(function(response){
     $("#login-register").hide();
-    $("#logo-container").append(response);
+    $(".form-container").append(response);
   });
   });
 }
@@ -33,9 +33,74 @@ var clickRegisterListener = function() {
   });
 
   request.done(function(response){
-    $("#logo-container").hide();
+    // $("#logo-container").hide();
     $("#login-register").hide();
-    $("#index-row").append(response);
+    $(".front-page-text").append(response);
   });
   });
+}
+
+
+//jQuery is required to run this code
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
 }
